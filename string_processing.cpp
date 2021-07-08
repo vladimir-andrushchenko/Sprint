ba8f8906c80233e92815f431af8d711df311f6b4
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2021-07-08 14:33:55 +0300

    chore

--- string_processing.cpp
+++ string_processing.cpp
@@ -17,4 +17,22 @@ std::vector<std::string> SplitIntoWords(const std::string& text) {
     return words;
 }
 
+std::vector<std::string_view> SplitIntoWords(std::string_view text) {
+    std::vector<std::string_view> result;
+
+    while (true) {
+        size_t space = text.find(' ');
+
+        result.push_back(text.substr(0, space));
+
+        if (space == text.npos) {
+            break;
+        } else {
+            text.remove_prefix(space + 1);
+        }
+    }
+    
+    return result;
+}
+
 } // string_processing
--- string_processing.h
+++ string_processing.h
@@ -5,8 +5,9 @@
 
 namespace string_processing {
 
+std::vector<std::string_view> SplitIntoWords(std::string_view text);
+
 std::vector<std::string> SplitIntoWords(const std::string& text);
 
 }
-
 
