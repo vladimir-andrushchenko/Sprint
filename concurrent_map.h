137fd0f579c9272a168e14540c6dcd6ff146968b
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2021-07-12 12:33:13 +0300

    added concurrent map

---
+++ concurrent_map.h
@@ -0,0 +1,50 @@
+#include <algorithm>
+#include <numeric>
+#include <vector>
+#include <string>
+#include <random>
+#include <mutex>
+#include <future>
+#include <map>
+#include <deque>
+
+using namespace std::string_literals;
+
+template <typename K, typename V>
+class ConcurrentMap {
+ public:
+  static_assert(std::is_integral_v<K>, "ConcurrentMap supports only integer keys");
+
+  struct Access {
+    std::lock_guard<std::mutex> guard;
+    V& ref_to_value;
+  };
+
+  explicit ConcurrentMap(size_t bucket_count) : BUCKET_COUNT(bucket_count) {
+    all_maps.resize(BUCKET_COUNT);
+  }
+
+  Access operator[](const K& key) {
+    size_t map_key = key % BUCKET_COUNT;
+    auto& curr_map = all_maps[map_key];
+    return Access{std::lock_guard(curr_map.bucket_mutex_), curr_map.bucket_map_[key]};
+  }
+
+  std::map<K, V> BuildOrdinaryMap() {
+    std::map<K, V> result;
+    for (auto& map : all_maps) {
+      for (const auto& [key, value] : map.bucket_map_) {
+        result[key] = operator[](key).ref_to_value;
+      }
+    }
+    return result;
+  }
+
+ private:
+  const size_t BUCKET_COUNT;
+  struct Bucket {
+    std::map<K, V> bucket_map_;
+    std::mutex bucket_mutex_;
+  };
+  std::deque<Bucket> all_maps;
+};
